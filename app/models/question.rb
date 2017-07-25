f471afac0b5863c023b8e888dab32dca8ede5380
naaaaya <naaya981024@gmail.com>
2017-07-25 16:42:35 +0900

    add association between survey and question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  belongs_to :survey
+  enum question_type: { text_field: 0, textarea: 1, checkbox: 2, radio_button: 3 }
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,4 @@
 class Survey < ApplicationRecord
+  has_many :questions
+  enum status: { draft: 0, published: 1, unlisted: 2 }
 end
