631411151d651870f21821c783fb947dec0941f6
naaaaya <naaya981024@gmail.com>
2017-07-27 19:41:49 +0900

    translate status and question_type

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ApplicationRecord
   has_many :questions
-  enum status: { draft: 0, published: 1, unlisted: 2 }
+  enum status: { '下書き': 0, '公開': 1, '非公開': 2 }
   validates :title, presence: true
 end
