797aa7a41be5fbf8d5906cf5ed98d9228d163461
naaaaya <naaya981024@gmail.com>
2017-07-29 16:12:12 +0900

    add survey and company relation

--- app/controllers/surveys_companies_controller.rb
+++ app/controllers/surveys_companies_controller.rb
@@ -1,6 +1,16 @@
 class SurveysCompaniesController < ApplicationController
 
   def create
-    binding.pry
+    @survey = Survey.find(params[:survey_id])
+    create_params[:company_ids].each do |company_id|
+      @survey.surveys_companies.create(company_id: company_id)
+    end
+  end
+
+
+  private
+
+  def create_params
+    params.require(:surveys_company).permit(company_ids:[])
   end
 end
