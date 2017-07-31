c04cad087617857510b2ca1ae5364919a0c1d69d
naaaaya <naaya981024@gmail.com>
2017-07-31 14:04:05 +0900

    fixed association btw survey and company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   has_many :users
-  has_many :surveys_companies
+  has_many :surveys, through: :surveys_company
+  belongs_to :surveys_company
   validates :name, presence: true
 end
