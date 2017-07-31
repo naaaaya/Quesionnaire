e2101f45ae1fbeac0bfeecef5641c36829fd59ad
naaaaya <naaya981024@gmail.com>
2017-08-01 00:48:18 +0900

    add surveys_companies

--- app/controllers/surveys_companies_controller.rb
+++ app/controllers/surveys_companies_controller.rb
@@ -5,18 +5,17 @@ class SurveysCompaniesController < ApplicationController
   end
 
   def create
-    binding.pry
     @survey = Survey.find(params[:survey_id])
     create_params[:company_ids].each do |company_id|
-      @survey.surveys_companies.create(company_id: company_id)
+      @survey.surveys_company.create(company_id: company_id)
     end
-    render survey_path(params[:survey_id])
+    render '/surveys/(params[:survey_id])'
   end
 
 
   private
 
   def create_params
-    params.require(:surveys_company).permit(company_ids:[])
+    params.permit(:survey_id, company_ids:[])
   end
 end
