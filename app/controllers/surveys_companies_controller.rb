fcc8d7ed01665efc373dd58fd91348681ed0f4ac
naaaaya <naaya981024@gmail.com>
2017-08-01 19:00:48 +0900

    add status when survey_companies are created

--- app/controllers/surveys_companies_controller.rb
+++ app/controllers/surveys_companies_controller.rb
@@ -5,12 +5,24 @@ class SurveysCompaniesController < ApplicationController
   end
 
   def create
-    survey_id = params[:survey_id]
-    @survey = Survey.find(survey_id)
-    create_params[:company_ids].each do |company_id|
-      @survey.surveys_company.create(company_id: company_id)
+    begin
+      survey_id = params[:survey_id]
+      @survey = Survey.find(survey_id)
+      @surveys_companies = []
+      create_params[:company_ids].each do |company_id|
+        surveys_company = @survey.surveys_company.new(company_id: company_id)
+        @surveys_companies << surveys_company
+      end
+      ActiveRecord::Base.transaction do
+        @survey.update!(status: 1)
+        @surveys_companies.each do |surveys_company|
+          surveys_company.save!
+        end
+      end
+      redirect_to survey_path(survey_id)
+    rescue Exception => e
+      redirect_to survey_path(survey_id)
     end
-    redirect_to survey_path(survey_id)
   end
 
 
