013921a29b5158a2b0ae622f1b187362c12b41cb
naaaaya <naaya981024@gmail.com>
2017-08-21 08:57:26 +0900

    add association btw surveys_user and answers

--- app/controllers/surveys_users_controller.rb
+++ app/controllers/surveys_users_controller.rb
@@ -7,6 +7,31 @@ class SurveysUsersController < ApplicationController
   end
 
   def create
-    binding.pry
+    survey = Survey.find(params[:survey_id])
+    surveys_user = survey.surveys_users.create(user_id: current_user.id)
+    create_params.each do |answer_param|
+      question = Question.find(answer_param[:id])
+      case question.question_type
+      when 'text_field', 'textarea'
+        surveys_user.text_answers.create(answer_param)
+      end
+    end
+  end
+
+private
+
+  def create_params
+    answer_params = []
+    params[:surveys_user].each do |question_id, answer|
+      case answer[:question_type]
+      when 'text_field', 'textarea'
+        answer_param = {id: question_id, description: answer[:description]}
+        answer_params << answer_param
+      when 'checkbox','radio_button'
+        answer_param = {id: question_id, answer: answer[:choise_ids]}
+        answer_params << answer_param
+      end
+    end
+    return answer_params
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :survey
   has_many :questions_choises
+  has_many :text_answers
+  has_many :choise_answers
   enum question_type: { 'text_field': 0, 'textarea': 1, 'checkbox': 2, 'radio_button': 3 }
   validates :description, presence: true
 end
