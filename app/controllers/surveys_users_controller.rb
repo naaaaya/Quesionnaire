2ca18b6009eae450928dc85a887ad47814148cb0
naaaaya <naaya981024@gmail.com>
2017-08-21 21:25:09 +0900

    add transaction

--- app/controllers/surveys_users_controller.rb
+++ app/controllers/surveys_users_controller.rb
@@ -1,29 +1,55 @@
 class SurveysUsersController < ApplicationController
+  before_action :set_survey, only:[:new, :create]
 
   def new
-    @survey = Survey.find(params[:survey_id])
     @surveys_user = @survey.surveys_users.new
     @questions = @survey.questions
   end
 
   def create
-    survey = Survey.find(params[:survey_id])
-    surveys_user = survey.surveys_users.create(user_id: current_user.id)
-    create_params.each do |answer_param|
-      question = Question.find(answer_param[:question_id])
-      case question.question_type
-      when 'text_field', 'textarea'
-        surveys_user.text_answers.create(answer_param)
-      when 'checkbox'
-        binding.pry
-        answer_param[:choise_ids].each do |choise_id|
-          surveys_user.choise_answers.create(question_id: answer_param[:question_id], questions_choise_id: choise_id)
-        end
-      when 'radio_button'
-        surveys_user.choise_answers.create(answer_param)
+    begin
+      case params[:commit]
+      when '下書き保存'
+        @surveys_user = @survey.surveys_users.new(user_id: current_user.id)
+      when '回答する'
+        @surveys_user = @survey.surveys_users.new(user_id: current_user.id, answered_flag: true)
       end
+      text_answers = []
+      choise_answers = []
+      create_params.each do |answer_param|
+        question = Question.find(answer_param[:question_id])
+        case question.question_type
+        when 'text_field', 'textarea'
+          text_answers << @surveys_user.text_answers.new(answer_param)
+        when 'checkbox'
+          answer_param[:choise_ids].each do |choise_id|
+            choise_answers << @surveys_user.choise_answers.new(question_id: answer_param[:question_id], questions_choise_id: choise_id)
+          end
+        when 'radio_button'
+          choise_answers << @surveys_user.choise_answers.new(answer_param)
+        end
+      end
+      ActiveRecord::Base.transaction do
+        @surveys_user.save!
+        text_answers.each do |answer|
+          answer.save!
+        end
+        choise_answers.each do |answer|
+          answer.save!
+        end
+      end
+      redirect_to surveys_path
+    rescue => e
+      @questions = @survey.questions
+      render new_survey_surveys_user
     end
   end
+
+  def set_survey
+    @survey_id = params[:survey_id]
+    @survey = Survey.find(@survey_id)
+  end
+
 
   private
 
@@ -38,7 +64,8 @@ class SurveysUsersController < ApplicationController
         answer_param = {question_id: question_id, choise_ids: answer[:choise_ids]}
         answer_params << answer_param
       when 'radio_button'
-        answer_param = {question_id: question_id, choise_id: answer[:choise_id]}
+        answer_param = {question_id: question_id, questions_choise_id: answer[:choise_id]}
+        answer_params << answer_param
       end
     end
     return answer_params
