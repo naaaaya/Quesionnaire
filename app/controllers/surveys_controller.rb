749bd5ff71855364e18681607df33f5e3922a2c5
naaaaya <naaya981024@gmail.com>
2017-08-23 18:30:07 +0900

    add show results function

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,4 +6,11 @@ class SurveysController < ApplicationController
     @draft_surveys = current_user.surveys_users.drafts
     @unanswered_surveys = Survey.never_answered(current_user)
   end
+
+  def show
+    redirect_to surveys_path if current_user.chief_flag == false && SurveysUser.try(:find_by, user_id: current_user, survey_id: params[:id]).try(:answered_flag) == false
+    @survey = Survey.find(params[:id])
+    @questions = @survey.questions
+    @users = current_user.company.users
+  end
 end
