680ffe1e6d9b2cfe2dd00169643c4c1d9c39fea9
naaaaya <naaya981024@gmail.com>
2017-08-23 14:53:57 +0900

    create views for admins/survey#show

--- app/controllers/admins/surveys_controller.rb
+++ app/controllers/admins/surveys_controller.rb
@@ -45,6 +45,7 @@ class Admins::SurveysController < ApplicationController
   def show
     @added_companies = @survey.companies
     @surveys_company = @survey.surveys_companies.build
+    @questions = @survey.questions
   end
 
   def edit
