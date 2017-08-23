a9c0f90b7c03252f299931082c0cc765d22c5fc7
naaaaya <naaya981024@gmail.com>
2017-08-23 22:22:22 +0900

    add delete employee function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    redirect_to authenticated_user_root unless current_user.chief_flag
+    @company = current_user.company
+    @users = @company.users
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    begin
+      ActiveRecord::Base.transaction do
+        @surveys_users = @user.try(:surveys_users)
+        if @surveys_users.present?
+          @surveys_users.each do |surveys_user|
+            surveys_user.text_answers.map{|answer| answer.destroy!} if surveys_user.text_answers
+            surveys_user.choise_answers.map{|answer| answer.destroy!} if surveys_user.choise_answers
+            surveys_user.destroy!
+          end
+        end
+        @user.destroy!
+      end
+      redirect_to users_path
+    rescue  => e
+    end
+  end
+end
