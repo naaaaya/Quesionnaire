933f42b456059db40ac5c0d7d36abc0483007aba
naaaaya <naaya981024@gmail.com>
2017-08-23 22:25:04 +0900

    fixed path name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    redirect_to authenticated_user_root unless current_user.chief_flag
+    redirect_to authenticated_user_root_path unless current_user.chief_flag
     @company = current_user.company
     @users = @company.users
   end
