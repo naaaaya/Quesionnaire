9c50a89c3268b8c88b526e23b266c1ed20d7800d
naaaaya <naaya981024@gmail.com>
2017-08-25 16:14:59 +0900

    add save/update user image function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :company_id, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :company_id, :image, :image_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation,:image, :image_cache])
   end
 
   def set_header_content
