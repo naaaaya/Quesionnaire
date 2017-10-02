7b743a67c16f8b3d4b0480b3b447d4e4912e7b2d
naaaaya <naaya981024@gmail.com>
2017-10-02 11:51:37 +0900

    use subject instead of before

--- spec/controllers/admins/surveys_controller_spec.rb
+++ spec/controllers/admins/surveys_controller_spec.rb
@@ -9,24 +9,25 @@ describe Admins::SurveysController do
   end
 
   describe 'GET #index' do
-    before do
-      get :index
-    end
+      subject { get :index }
     context 'with specific surveys' do
       it 'assigns the draft surveys to @draft_surveys' do
         draft_surveys = create_list(:survey, 2, status: 0)
+        subject
         expect(assigns(:draft_surveys)).to eq(draft_surveys)
       end
       it 'assigns the published surveys to @published_surveys' do
         published_surveys = create_list(:survey, 2, status: 1)
+        subject
         expect(assigns(:published_surveys)).to eq(published_surveys)
       end
       it 'assigns the unlisted surveys to @unlisted_surveys' do
         unlisted_surveys = create_list(:survey, 2, status: 2)
+        subject
         expect(assigns(:unlisted_surveys)).to eq(unlisted_surveys)
       end
     end
-    it { expect(response).to render_template :index }
+    it { expect(subject).to render_template :index }
   end
 
   describe 'GET #new' do
@@ -39,13 +40,13 @@ describe Admins::SurveysController do
   describe 'GET #show' do
     it 'renders the :show template' do
       survey.published!
-      get :show, params: { id: survey }
+      get :show, params: { id: survey.id }
       expect(response).to render_template :show
     end
   end
 
   describe 'GET #edit' do
-    subject { get :edit, params: { id: survey } }
+    subject { get :edit, params: { id: survey.id } }
     it { expect(subject).to render_template :edit }
 
     it 'redirects to index unless survey is draft' do
@@ -80,18 +81,18 @@ describe Admins::SurveysController do
       ]}
     context 'with valid attributes' do
       it "changes question's attributes" do
-        patch :update, params: { id: survey, survey: attributes_for(:survey, title: 'aaa', description: 'bbb'), questions: [ { id: question.id, description: 'aaa' } ] }
+        patch :update, params: { id: survey.id, survey: attributes_for(:survey, title: 'aaa', description: 'bbb'), questions: [ { id: question.id, description: 'aaa' } ] }
         question.reload
         expect(question.description).to eq('aaa')
       end
       it 'redirects to admins_survey_path' do
-         patch :update, params: { id: survey, survey: attributes_for(:survey), questions: questions }
+         patch :update, params: { id: survey.id, survey: attributes_for(:survey), questions: questions }
          expect(response).to redirect_to admins_survey_path
       end
     end
 
     context 'with invalid attributes' do
-      let(:params) { {id: survey, survey: attributes_for(:survey, title: nil), questions: questions} }
+      let(:params) { {id: survey.id, survey: attributes_for(:survey, title: nil), questions: questions} }
       before do
         patch :update, params: params
       end
@@ -100,7 +101,7 @@ describe Admins::SurveysController do
     end
 
     it 'unlist survey' do
-      patch :update, params: { id: survey, survey: attributes_for(:survey), unlist_survey: "非表示" }
+      patch :update, params: { id: survey.id, survey: attributes_for(:survey), unlist_survey: "非表示" }
       survey.reload
       expect(survey.unlisted?).to be true
     end
@@ -112,15 +113,16 @@ describe Admins::SurveysController do
     let!(:questions_choise) { create(:questions_choise, question_id: question.id) }
     let!(:surveys_user) { survey.surveys_users.create(user_id: user.id) }
     let!(:choise_answer) { questions_choise.choise_answers.create(surveys_user_id: surveys_user.id, question_id: question.id) }
+    subject { delete :destroy, params: { id: survey.id } }
 
     it 'redirects to admins_surveys_path' do
-      delete :destroy, params: { id: survey }
+      subject
       expect(response).to redirect_to admins_surveys_path
     end
 
     it 'renders to admins_survey_path when destroy fails' do
       allow_any_instance_of(Survey).to receive(:destroy!).and_raise(ActiveRecord::RecordNotSaved, "error")
-      delete :destroy, params: { id: survey }
+      subject
       expect(response).to render_template :edit
     end
   end
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -7,12 +7,13 @@ describe SurveysController do
   end
   describe 'GET #index' do
     let(:surveys) { create_list(:survey, 2, status: 1) }
+    subject { get :index }
     it 'assigns answered surveys to @answered_surveys' do
       answered_surveys = []
       surveys.each do |survey|
         answered_surveys << survey.surveys_users.create(user_id: user.id, answered_flag: true)
       end
-      get :index
+      subject
       expect(assigns(:answered_surveys)).to eq answered_surveys
     end
     it 'assigns draft surveys to @draft_surveys' do
@@ -20,25 +21,25 @@ describe SurveysController do
       surveys.each do |survey|
         draft_surveys << survey.surveys_users.create(user_id: user.id, answered_flag: false)
       end
-      get :index
+      subject
       expect(assigns(:draft_surveys)).to eq draft_surveys
     end
     it 'assigns unanswered surveys to @unanswered_surveys' do
       surveys.each do |survey|
         survey.surveys_companies.create(company_id: user.company.id)
       end
-      get :index
+      subject
       expect(assigns(:unanswered_surveys)).to eq surveys
     end
   end
 
   describe 'GET #show' do
     let(:survey) { create(:survey) }
-    before do
-      get :show, params: { id: survey }
-    end
-    it { expect(assigns(:survey)).to eq survey }
-    it { expect(response).to redirect_to surveys_path }
+    subject { get :show, params: { id: survey.id } }
+    it { subject
+        expect(assigns(:survey)).to eq survey
+        }
+    it { expect(subject).to redirect_to surveys_path }
   end
 
 end
